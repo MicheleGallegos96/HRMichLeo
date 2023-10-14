5e565687888cf47bdeee947e5fdf66ef3a839a20
Michele Gallegos <ped.gallegosguerr@stud.uniroma3.it>
2023-10-14 12:46:33 +0200

    colloquiante e lavoro controller

---
+++ HRMichLeo/Server/Controllers/DatiColloquianteController.cs
@@ -0,0 +1,67 @@
+﻿using HRMichLeo.Server.Interfaces;
+using HRMichLeo.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMichLeo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatiColloquianteController : ControllerBase
+    {
+        
+        private readonly IRepositoryBase<DatiColloquiante> _datiColloquianteRepository;
+
+        public DatiColloquianteController(IRepositoryBase<DatiColloquiante> datiColloquianteRepository)
+        {
+            _datiColloquianteRepository = datiColloquianteRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<DatiColloquiante>> GetAll()
+        {
+            var risultato = await _datiColloquianteRepository.GetAll();
+            return Ok(risultato);
+        }
+
+        [HttpGet("Id")]
+        public async Task<ActionResult<DatiColloquiante>> GetById(Guid id)
+        {
+            var risultato = await _datiColloquianteRepository.GetById(id);
+            return Ok(risultato);
+        }
+
+        [HttpPost("Create")]
+
+        public async Task<ActionResult<DatiColloquiante>> Post(DatiColloquiante colloquiante)
+        {
+            await _datiColloquianteRepository.Create(colloquiante);
+            return CreatedAtAction("GetById", new { id = colloquiante.Id }, colloquiante);
+
+        }
+
+        [HttpDelete("Delete")]
+
+        public async Task<ActionResult<DatiColloquiante>> Delete(Guid id)
+        {
+            await _datiColloquianteRepository.Delete(id);
+            return Ok();
+
+        }
+
+        [HttpPut("Id")]
+
+        public async Task<ActionResult<DatiColloquiante>> Put(Guid id, DatiColloquiante datiColloquiante)
+        {
+            if (id != datiColloquiante.Id)
+            {
+                return BadRequest();
+            }
+            await _datiColloquianteRepository.Update(datiColloquiante);
+            return Ok(datiColloquiante);
+
+        }
+
+    }
+}
+
---
+++ HRMichLeo/Server/Controllers/EsperienzaLavorativaController.cs
@@ -0,0 +1,66 @@
+﻿using HRMichLeo.Server.Interfaces;
+using HRMichLeo.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMichLeo.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EsperienzaLavorativaController : ControllerBase
+    {
+        private readonly IRepositoryBase<EsperienzaLavorativa> _esperienzeLavorativaRepository;
+
+        public EsperienzaLavorativaController(IRepositoryBase<EsperienzaLavorativa> esperienzaLavorativaRepository)
+        {
+            _esperienzeLavorativaRepository = esperienzaLavorativaRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<EsperienzaLavorativa>> GetAll()
+        {
+            var risultato = await _esperienzeLavorativaRepository.GetAll();
+            return Ok(risultato);
+        }
+
+        [HttpGet("Id")]
+        public async Task<ActionResult<EsperienzaLavorativa>> GetById(Guid id)
+        {
+            var risultato = await _esperienzeLavorativaRepository.GetById(id);
+            return Ok(risultato);
+        }
+
+        [HttpPost("Create")]
+
+        public async Task<ActionResult<EsperienzaLavorativa>> Post(EsperienzaLavorativa lavoro)
+        {
+            await _esperienzeLavorativaRepository.Create(lavoro);
+            return CreatedAtAction("GetById", new { id = lavoro.Id }, lavoro);
+
+        }
+
+        [HttpDelete("Delete")]
+
+        public async Task<ActionResult<EsperienzaLavorativa>> Delete(Guid id)
+        {
+            await _esperienzeLavorativaRepository.Delete(id);
+            return Ok();
+
+        }
+
+        [HttpPut("Id")]
+
+        public async Task<ActionResult<EsperienzaLavorativa>> Put(Guid id, EsperienzaLavorativa esperienzaLavorativa)
+        {
+            if (id != esperienzaLavorativa.Id)
+            {
+                return BadRequest();
+            }
+            await _esperienzeLavorativaRepository.Update(esperienzaLavorativa);
+            return Ok(esperienzaLavorativa);
+
+        }
+
+    }
+}
+
